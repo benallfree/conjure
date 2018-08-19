e159e9cac66ca2514d5cdc91069ecf526ad135b9
Ben Allfree <ben@benallfree.com>
2018-08-19 07:33:49 -0700

    Update routing strategy

--- resources/assets/js/components/Root.js
+++ resources/assets/js/components/Root.js
@@ -1,10 +1,17 @@
 import React, { Component } from 'react'
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
-import { Dashboard } from './Dashboard'
+import _ from 'lodash'
+import { routes } from '../routes'
 
 const Root = () => (
   <Router>
-    <Route exact path="/" component={Dashboard} />
+    <div>
+      <Route exact path="/" component={routes['/']} />
+      {_.map(
+        routes,
+        (v, k) => (k === '/' ? null : <Route key={k} path={k} component={v} />),
+      )}
+    </div>
   </Router>
 )
 

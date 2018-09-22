7df1ddc08e7c7b500e5cf57dffd495c62f664575
Ben Allfree <ben@benallfree.com>
2018-09-22 03:56:07 -0700

    mix fix

--- webpack.mix.js
+++ webpack.mix.js
@@ -2,6 +2,8 @@ const mix = require('laravel-mix')
 
 mix
   .react('resources/assets/js/app.js', 'public/js')
-  .version()
   .sass('resources/assets/sass/app.scss', 'public/css')
-  .version()
+
+if (mix.inProduction()) {
+  mix.version()
+}

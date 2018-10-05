e4fd2eee4e1b1ee9b311804bde1013946e9c02ea
Ben Allfree <ben@benallfree.com>
2018-10-04 22:10:38 -0700

    BP: add urrency field

--- resources/assets/js/components/Kit/formFuncs.js
+++ resources/assets/js/components/Kit/formFuncs.js
@@ -11,36 +11,16 @@ function integerField(opts = {}) {
   const max = opts.max || 0
   const digits = `${max}`.length
   const mask = '0'.repeat(digits)
-  return _.extend(
-    {
-      defaultValue: ({ context }) => opts.min || 0,
-      inputLabel: () => 'qty',
-      format: ({ value }) => numeral(parseInt(value, 0)).format(mask),
-      mask,
-      unmask: /[^\d]/g,
-      validate: ({ value }) => value >= min && value <= max,
-    },
-    opts,
-  )
-}
-
-function floatField(opts = {}) {
-  const min = opts.min || 0.0
-  const max = opts.max || 0.0
-  const precision = opts.precision || 2
-  const left = '0'.repeat(`${max}`.length)
-  const right = '0'.repeat(precision)
-  const mask = `${left}.${right}`
-  return _.extend(
-    {
-      defaultValue: ({ context }) => opts.min || 0,
-      format: ({ value }) => numeral(parseFloat(value)).format(mask),
-      mask,
-      unmask: ({ value }) => value.replace(/[^\d.]/g, '').replace(/\.$/, ''),
-      validate: ({ value }) => value >= min && value <= max,
-    },
-    opts,
-  )
+  return {
+    defaultValue: ({ context }) => opts.min || 0,
+    inputLabel: () => 'qty',
+    format: ({ value }) => numeral(parseInt(value, 0)).format(mask),
+    mask,
+    unmask: /[^\d]/g,
+    validate: ({ value }) =>
+      parseInt(value, 0) >= min && parseInt(value, 0) <= max,
+    ...opts,
+  }
 }
 
 function quantityField(opts = {}) {
@@ -56,35 +36,58 @@ function quantityField(opts = {}) {
   })
 }
 
-function rateField(opts = {}) {
+function floatField(opts = {}) {
+  const min = opts.min || 0.0
+  const max = opts.max || 0.0
+  const precision = opts.precision || 2
+  const left = '0'.repeat(`${max}`.length)
+  const right = '0'.repeat(precision)
+  const mask = `${left}.${right}`
+  return {
+    defaultValue: ({ context }) => opts.min || 0,
+    format: ({ value }) => numeral(parseFloat(value)).format(mask),
+    mask,
+    unmask: ({ value }) => value.replace(/[^\d.]/g, '').replace(/\.$/, ''),
+    validate: ({ value }) =>
+      parseFloat(value) >= min && parseFloat(value) <= max,
+    ...opts,
+  }
+}
+
+function currencyField(opts = {}) {
   return floatField({
     min: 0,
-    max: 5,
-    precision: 4,
+    max: 1000,
+    precision: 2,
     inputLabel: () => '$',
     ...opts,
   })
 }
 
+function rateField(opts = {}) {
+  return currencyField({
+    min: 0,
+    max: 5,
+    precision: 4,
+    ...opts,
+  })
+}
+
 function phoneField(opts = {}) {
-  return _.extend(
-    {
-      mask: '(111) 555-1212',
-      unmask: /[^\d]/g,
-      validate: /\d{10}/,
-    },
-    opts,
-  )
+  return {
+    mask: '(111) 555-1212',
+    unmask: /[^\d]/g,
+    validate: /\d{10}/,
+    ...opts,
+  }
 }
 
 function toggleField(opts = {}) {
-  return _.extend(
-    {
-      type: 'Toggle',
-      defaultValue: false,
-    },
-    opts,
-  )
+  return {
+    type: 'Toggle',
+    defaultValue: false,
+    ...opts,
+  }
 }
 
 function emailField(opts = {}) {
@@ -99,30 +102,24 @@ function emailField(opts = {}) {
 }
 
 function sectionField(opts = {}) {
-  return _.extend(
-    {
-      type: 'Section',
-    },
-    opts,
-  )
+  return {
+    type: 'Section',
+    ...opts,
+  }
 }
 
 function dropdownField(opts = {}) {
-  return _.extend(
-    {
-      type: 'Dropdown',
-    },
-    opts,
-  )
+  return {
+    type: 'Dropdown',
+    ...opts,
+  }
 }
 
 function divField(opts = {}) {
-  return _.extend(
-    {
-      type: 'Div',
-    },
-    opts,
-  )
+  return {
+    type: 'Div',
+    ...opts,
+  }
 }
 
 function createMask(s) {
@@ -142,6 +139,7 @@ export {
   emailField,
   toggleField,
   phoneField,
+  currencyField,
   rateField,
   quantityField,
   floatField,

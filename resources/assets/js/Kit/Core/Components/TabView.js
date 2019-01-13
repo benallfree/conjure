4825bcda4d97b7b897fe5c8d903da252bdabdae3
Ben Allfree <ben@benallfree.com>
2019-01-13 10:10:33 -0800

    Conjure: printing support

--- resources/assets/js/Kit/Core/Components/TabView.js
+++ resources/assets/js/Kit/Core/Components/TabView.js
@@ -3,26 +3,29 @@ import _ from 'lodash'
 import { Button } from 'semantic-ui-react'
 import { RouteButton } from './RouteButton'
 import { RouteRenderer } from './RouteRenderer'
+import { NoPrint } from './NoPrint'
 
 const TabView = props => {
   const { route, buttonProps, ...rest } = props
 
   return (
-    <div>
-      <Button.Group style={{ marginBottom: 20 }} {...buttonProps}>
-        {_.map(route.routes, (r, k) => {
-          const { fullPath, menu } = r
-          const { decorators, title } = menu
-          const MyButton = _.reduce(decorators, (c, d) => d(c), RouteButton)
-          return (
-            <MyButton {...rest} key={fullPath} route={r}>
-              {title}
-            </MyButton>
-          )
-        })}
-      </Button.Group>
+    <React.Fragment>
+      <NoPrint>
+        <Button.Group style={{ marginBottom: 20 }} {...buttonProps}>
+          {_.map(route.routes, (r, k) => {
+            const { fullPath, menu } = r
+            const { decorators, title } = menu
+            const MyButton = _.reduce(decorators, (c, d) => d(c), RouteButton)
+            return (
+              <MyButton {...rest} key={fullPath} route={r}>
+                {title}
+              </MyButton>
+            )
+          })}
+        </Button.Group>
+      </NoPrint>
       <RouteRenderer {...rest} routes={route.routes} />
-    </div>
+    </React.Fragment>
   )
 }
 
--- resources/assets/js/Kit/Core/Components/TopNav.js
+++ resources/assets/js/Kit/Core/Components/TopNav.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
 import _ from 'lodash'
 import { Menu } from 'semantic-ui-react'
+import { NoPrint } from './NoPrint'
 
 const MenuItemGroup = props => {
   const { position, routes, activeItem } = props
@@ -30,23 +31,25 @@ const TopNav = props => {
   const { match, routes } = props
   const menuRoutes = _.filter(routes, ({ menu }) => typeof menu !== 'undefined')
   return (
-    <Menu borderless style={{ marginBottom: 20 }}>
-      <Menu.Item header as={Link} to={routes.home()}>
-        {process.env.APP_NAME}
-      </Menu.Item>
-      <MenuItemGroup
-        routes={menuRoutes}
-        position={null}
-        activeItem={match.url}
-      />
-      <Menu.Menu position="right">
+    <NoPrint>
+      <Menu borderless style={{ marginBottom: 20 }}>
+        <Menu.Item header as={Link} to={routes.home()}>
+          {process.env.APP_NAME}
+        </Menu.Item>
         <MenuItemGroup
           routes={menuRoutes}
-          position="right"
+          position={null}
           activeItem={match.url}
         />
-      </Menu.Menu>
-    </Menu>
+        <Menu.Menu position="right">
+          <MenuItemGroup
+            routes={menuRoutes}
+            position="right"
+            activeItem={match.url}
+          />
+        </Menu.Menu>
+      </Menu>
+    </NoPrint>
   )
 }
 
